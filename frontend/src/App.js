afaa93bec36fb4765fa268d6f8cca631be2cd2df
theDRB123 <dhrbhanushali123@gmail.com>
2023-09-28 00:38:41 +0530

    implemented a basic api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/home")
+      .then((res) => res.text())
+      .then((data) => setData(data))
+  }, [])
+
   return (
     <div>
-      HELLO There!
+        {data}
     </div>
   );
 }
