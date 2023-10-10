f4d4df09fafccf4d8de84b2908fb77c9b1a394a0
theDRB123 <dhrbhanushali123@gmail.com>
2023-10-10 14:00:46 +0530

    Basic Frontend Setup Completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,309 @@
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import "./index.css"
 
-function App() {
+const App = () => {
   const [data, setData] = useState(null);
 
-  useEffect(() => {
-    fetch("http://localhost:4000/home")
-      .then((res) => res.text())
-      .then((data) => setData(data))
-  }, [])
+  // useEffect(() => {
+  //   fetch("http://localhost:4000/home")
+  //     .then((res) => res.text())
+  //     .then((data) => setData(data))
+  // }, [])
 
   return (
-    <div>
-        {data}
+    <div className="container min-h-[100vh]" style={
+      {
+        minWidth: "100%",
+        background: "linear-gradient(to bottom, #29B6C960, #f2f2f25f)"
+      }
+    } >
+
+      <div className='SideBar md:visible invisible flex-col h-[100vh] z-50 md:w-[33%] w-[100%] p-4 pt-[150px] md:pt-[5vh] fixed md:top-1/2 md:-translate-y-1/2  md:left-[67%] md:-translate-x-100 rounded-tl-[30px]' style={{
+        background: "linear-gradient(180deg, #89D2D5 0%, rgba(73, 170, 173, 0.48) 19.09%, rgba(14, 133, 137, 0.00) 100%",
+        'backdrop-filter': 'blur(15px)'
+      }}>
+        <div className='SearchBar flex md:h-[45px] h-[60px] w-[100%] bg-white rounded-[15px] mb-[40px] md:mb-[3vh] pl-4'>
+            <label htmlFor="search" className='label invisible absolute' >SearchBar</label>
+            <input type="text" name="search" placeholder='Search' id="search" className='search m-auto ml-0'/>
+        </div>
+
+        <div className='FilterContainer md:h-[150px] h-[200px] w-[100%] bg-white rounded-[15px] mb-[40px] md:mb-[3vh] p-3'>
+          <p className='text-[#828282] mb-1 ml-1'>Filter</p>
+          <div className='FilterOptionContainer'>
+            <div className='FilterSem md:h-[30px] h-[45px] w-[100%] bg-[#567b86bc] md:mb-1 mb-2 rounded-[10px] flex pl-3 items-center'>
+              <label htmlFor="FilterSem" className='w-[20%] text-[#ffffff]'>Sem</label>
+              <select name="FilterSem" id="FilterSem" className='w-[75%] flex text-center justify-end bg-[#00000000] text-white'>
+                <option value="0">All</option>
+                <option value="1">Sem1</option>
+                <option value="2">Sem2</option>
+                <option value="3">Sem3</option>
+                <option value="4">Sem4</option>
+                <option value="5">Sem5</option>
+                <option value="6">Sem6</option>
+                <option value="7">Sem7</option>
+                <option value="8">Sem8</option>
+              </select>
+            </div>
+            <div className='FilterDepartment md:h-[30px] h-[45px] w-[100%] bg-[#567b86bc] md:mb-1 mb-2 rounded-[10px] flex pl-3 items-center'>
+            <label htmlFor="FilterDepartment" className='w-[20%] text-[#ffffff]'>Dept</label>
+              <select name="FilterDepartment" id="FilterDepartment" className='w-[75%] flex text-center justify-end bg-[#00000000] text-white'>
+                <option value="CSE">CSE</option>
+                <option value="ECE">ECE</option>
+                <option value="ME">ME</option>
+              </select>
+            </div>
+            <div className='FilterCredits md:h-[30px] h-[45px] w-[100%] bg-[#567b86bc] md:mb-1 mb-2 rounded-[10px] flex pl-3 items-center'>
+            <label htmlFor="FilterCredits" className='w-[20%] text-[#ffffff]'>Credits</label>
+              <select name="FilterCredits" id="FilterCredits" className='w-[75%] flex text-center justify-end bg-[#00000000] text-white'>
+                <option value="0">All</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div className="SeperateBy md:h-[150px] h-[200px] w-[100%] bg-white rounded-[15px] mb-[40px] p-3">
+          <p className='text-[#828282] mb-1 ml-1'>Seperate By</p>
+          <div className='SeparateByOptionContainer flex flex-wrap'>
+            <div className='SepOption bg-[#567b86bc] p-1 rounded-md text-white mr-[5px] mt-[5px]' value='sem'>
+              Sem
+            </div>
+            <div className='SepOption bg-[#567b86bc] p-1 rounded-md text-white mr-[5px] mt-[5px]' value='dept'>
+              Department
+            </div>
+            <div className='SepOption bg-[#567b86bc] p-1 rounded-md text-white mr-[5px] mt-[5px]' value='credit'>
+              Credits
+            </div>
+            <div className='SepOption bg-[#567b86bc] p-1 rounded-md text-white mr-[5px] mt-[5px]' value='year'>
+              Year
+            </div>
+            <div className='SepOption bg-[#567b86bc] p-1 rounded-md text-white mr-[5px] mt-[5px]' value='faculty'>
+              Faculty
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
+      <div className="TitleContainer md:flex w-[100%] h-auto pb-5 left-0 top-[0px] sticky bg-[#f2f2f25f] backdrop-blur-sm mb-3"
+        style={
+          {
+            background: "linear-gradient(to bottom, #29B6C9, #f2f2f25f)",
+          }}
+      >
+        <div className='TitleBox flex items-center justify-center h-[8vh] md:h-[10vh] md:w-[45%] md:ml-[4vw] bg-stone-50 md:rounded-bl-[5px] md:rounded-br-[5px] shadow overflow-hidden' >
+          <div className="TitleText p-3 text-center text-slate-500 text-3xl ld:text-4xl font-medium font-'Roboto' ">
+            IIIDMJ Course Collection
+          </div>
+        </div>
+        <div className='NavBar flex items-center sm:items-center h-[90%] md:h-[85%] md:w-[45%] '>
+
+        </div>
+
+      </div>
+
+
+      <div className='ContentL0 flex flex-wrap h-auto bg-[#e3e7ea00] p-3 pt-0'>
+
+        <div className='ContentL1 lg:w-[100%] md:w-[100%] sm:w-[100%] w-[100%] h-[auto] max-h-[310px] overflow-auto flex-col bg-[#80a3a7bc] rounded-[3px] p-[10px] pt-0 mb-[20px]' style={
+          {
+            // background: "linear-gradient(to bottom, #7b9a9f, #7b9a9f50 ,#f2f2f25f ) ",
+          }}
+        >
+
+
+          <div className='ContentL1Title flex items-center justify-left w-[100%] pl-[30%] lg:h-[40px] md:h-[40px] sm:h-[9vh]  lg:text-l sm:text-l text-white font-roboto font-medium m-auto'>
+            <p>Sem 1</p>
+          </div>
+
+          <div className='ContentL1.5 flex w-[100%] h-[260px] overflow-auto lg:pb-3 md:pb-3'>
+
+            <div className='ContentL2 flex flex-wrap min-w-[200px] h-[100%] sm:w-[200px] md:min-w-[250px] bg-[#F5F9F9]  rounded-[2px] p-3 mr-[20px]'>
+              <div className='CourseTitle text-[#344148c1] text-left w-[76%] text-l font-medium font-"Roboto Flex"  ' >
+                Mathematics 1
+              </div>
+              <div className='CourseCode text-[#798389] font-medium font-"Roboto Flex"'>
+                NS1001
+              </div>
+              <div className='CourseDescription w-[100%] text-[#505C62] font-light text-sm'>
+                Real Numbers , Functions , Sequences , Limit and Continuity, Differentiation
+              </div>
+              <div className='CourseCredits self-end w-[100%] text-right'>
+                4 Credits
+              </div>
+            </div>
+
+            <div className='ContentL2 flex flex-wrap min-w-[200px] h-[100%] sm:w-[200px] md:min-w-[250px] bg-[#F5F9F9]  rounded-[2px] p-3 mr-[20px]'>
+              <div className='CourseTitle text-[#344148c1] text-left w-[76%] text-l font-medium font-"Roboto Flex"  ' >
+                Mathematics 1
+              </div>
+              <div className='CourseCode text-[#798389] font-medium font-"Roboto Flex"'>
+                NS1001
+              </div>
+              <div className='CourseDescription w-[100%] text-[#505C62] font-light text-sm'>
+                Real Numbers , Functions , Sequences , Limit and Continuity, Differentiation
+              </div>
+              <div className='CourseCredits self-end w-[100%] text-right'>
+                4 Credits
+              </div>
+            </div>
+
+            <div className='ContentL2 flex flex-wrap min-w-[200px] h-[100%] sm:w-[200px] md:min-w-[250px] bg-[#F5F9F9]  rounded-[2px] p-3 mr-[20px]'>
+              <div className='CourseTitle text-[#344148c1] text-left w-[76%] text-l font-medium font-"Roboto Flex"  ' >
+                Mathematics 1
+              </div>
+              <div className='CourseCode text-[#798389] font-medium font-"Roboto Flex"'>
+                NS1001
+              </div>
+              <div className='CourseDescription w-[100%] text-[#505C62] font-light text-sm'>
+                Real Numbers , Functions , Sequences , Limit and Continuity, Differentiation
+              </div>
+              <div className='CourseCredits self-end w-[100%] text-right'>
+                4 Credits
+              </div>
+            </div>
+
+
+            <div className='ContentL2 flex flex-wrap min-w-[200px] h-[100%] sm:w-[200px] md:min-w-[250px] bg-[#F5F9F9]  rounded-[2px] p-3 mr-[20px]'>
+              <div className='CourseTitle text-[#344148c1] text-left w-[76%] text-l font-medium font-"Roboto Flex"  ' >
+                Mathematics 1
+              </div>
+              <div className='CourseCode text-[#798389] font-medium font-"Roboto Flex"'>
+                NS1001
+              </div>
+              <div className='CourseDescription w-[100%] text-[#505C62] font-light text-sm'>
+                Real Numbers , Functions , Sequences , Limit and Continuity, Differentiation
+              </div>
+              <div className='CourseCredits self-end w-[100%] text-right'>
+                4 Credits
+              </div>
+            </div>
+
+            <div className='ContentL2 flex flex-wrap min-w-[200px] h-[100%] sm:w-[200px] md:min-w-[250px] bg-[#F5F9F9]  rounded-[2px] p-3 mr-[20px]'>
+              <div className='CourseTitle text-[#344148c1] text-left w-[76%] text-l font-medium font-"Roboto Flex"  ' >
+                Mathematics 1
+              </div>
+              <div className='CourseCode text-[#798389] font-medium font-"Roboto Flex"'>
+                NS1001
+              </div>
+              <div className='CourseDescription w-[100%] text-[#505C62] font-light text-sm'>
+                Real Numbers , Functions , Sequences , Limit and Continuity, Differentiation
+              </div>
+              <div className='CourseCredits self-end w-[100%] text-right'>
+                4 Credits
+              </div>
+            </div>
+
+          </div>
+
+
+        </div>
+
+        <div className='ContentL1 lg:w-[100%] md:w-[100%] sm:w-[100%] w-[100%] h-[auto] max-h-[310px] overflow-auto flex-col bg-[#80a3a7bc] rounded-[3px] p-[10px] pt-0' style={
+          {
+            // background: "linear-gradient(to bottom, #7b9a9f, #7b9a9f50 ,#f2f2f25f ) ",
+          }}
+        >
+
+          <div className='ContentL1Title flex items-center justify-left w-[100%] pl-[30%] lg:h-[40px] md:h-[40px] sm:h-[9vh]  lg:text-l sm:text-l text-white font-roboto font-medium m-auto'>
+            <p>Sem 1</p>
+          </div>
+
+          <div className='ContentL1.5 flex w-[100%] h-[260px] overflow-auto lg:pb-3 md:pb-3'>
+
+            <div className='ContentL2 flex flex-wrap min-w-[200px] h-[100%] sm:w-[200px] md:min-w-[250px] bg-[#F5F9F9]  rounded-[2px] p-3 mr-[20px]'>
+              <div className='CourseTitle text-[#344148c1] text-left w-[76%] text-l font-medium font-"Roboto Flex"  ' >
+                Mathematics 1
+              </div>
+              <div className='CourseCode text-[#798389] font-medium font-"Roboto Flex"'>
+                NS1001
+              </div>
+              <div className='CourseDescription w-[100%] text-[#505C62] font-light text-sm'>
+                Real Numbers , Functions , Sequences , Limit and Continuity, Differentiation
+              </div>
+              <div className='CourseCredits self-end w-[100%] text-right'>
+                4 Credits
+              </div>
+            </div>
+
+            <div className='ContentL2 flex flex-wrap min-w-[200px] h-[100%] sm:w-[200px] md:min-w-[250px] bg-[#F5F9F9]  rounded-[2px] p-3 mr-[20px]'>
+              <div className='CourseTitle text-[#344148c1] text-left w-[76%] text-l font-medium font-"Roboto Flex"  ' >
+                Mathematics 1
+              </div>
+              <div className='CourseCode text-[#798389] font-medium font-"Roboto Flex"'>
+                NS1001
+              </div>
+              <div className='CourseDescription w-[100%] text-[#505C62] font-light text-sm'>
+                Real Numbers , Functions , Sequences , Limit and Continuity, Differentiation
+              </div>
+              <div className='CourseCredits self-end w-[100%] text-right'>
+                4 Credits
+              </div>
+            </div>
+
+            <div className='ContentL2 flex flex-wrap min-w-[200px] h-[100%] sm:w-[200px] md:min-w-[250px] bg-[#F5F9F9]  rounded-[2px] p-3 mr-[20px]'>
+              <div className='CourseTitle text-[#344148c1] text-left w-[76%] text-l font-medium font-"Roboto Flex"  ' >
+                Mathematics 1
+              </div>
+              <div className='CourseCode text-[#798389] font-medium font-"Roboto Flex"'>
+                NS1001
+              </div>
+              <div className='CourseDescription w-[100%] text-[#505C62] font-light text-sm'>
+                Real Numbers , Functions , Sequences , Limit and Continuity, Differentiation
+              </div>
+              <div className='CourseCredits self-end w-[100%] text-right'>
+                4 Credits
+              </div>
+            </div>
+
+
+            <div className='ContentL2 flex flex-wrap min-w-[200px] h-[100%] sm:w-[200px] md:min-w-[250px] bg-[#F5F9F9]  rounded-[2px] p-3 mr-[20px]'>
+              <div className='CourseTitle text-[#344148c1] text-left w-[76%] text-l font-medium font-"Roboto Flex"  ' >
+                Mathematics 1
+              </div>
+              <div className='CourseCode text-[#798389] font-medium font-"Roboto Flex"'>
+                NS1001
+              </div>
+              <div className='CourseDescription w-[100%] text-[#505C62] font-light text-sm'>
+                Real Numbers , Functions , Sequences , Limit and Continuity, Differentiation
+              </div>
+              <div className='CourseCredits self-end w-[100%] text-right'>
+                4 Credits
+              </div>
+            </div>
+
+            <div className='ContentL2 flex flex-wrap min-w-[200px] h-[100%] sm:w-[200px] md:min-w-[250px] bg-[#F5F9F9]  rounded-[2px] p-3 mr-[20px]'>
+              <div className='CourseTitle text-[#344148c1] text-left w-[76%] text-l font-medium font-"Roboto Flex"  ' >
+                Mathematics 1
+              </div>
+              <div className='CourseCode text-[#798389] font-medium font-"Roboto Flex"'>
+                NS1001
+              </div>
+              <div className='CourseDescription w-[100%] text-[#505C62] font-light text-sm'>
+                Real Numbers , Functions , Sequences , Limit and Continuity, Differentiation
+              </div>
+              <div className='CourseCredits self-end w-[100%] text-right'>
+                4 Credits
+              </div>
+            </div>
+
+          </div>
+
+
+        </div>
+
+
+
+      </div>
+
+
+
     </div>
   );
 }
---
+++ frontend/src/styling.js
@@ -0,0 +1,49 @@
+<div className="Frame1 w-[1920px] h-[1080px] relative bg-gray-100">
+  <div className="Infoboard w-[1920px] h-[1368px] left-0 top-[-14px] absolute">
+    <div className="InfoboardContainer w-[1920px] h-[1093px] left-0 top-0 absolute bg-gray-400" />
+    <div className="InfoboardSeperateContainer w-[1880px] h-[577px] left-[20px] top-[185px] absolute">
+      <div className="Info w-[481px] h-[456px] left-[31px] top-[85px] absolute bg-slate-50 rounded-[20px]" />
+      <div className="Info w-[481px] h-[456px] left-[547px] top-[85px] absolute bg-slate-50 rounded-[20px]" />
+      <div className="Info w-[481px] h-[456px] left-[1062px] top-[85px] absolute bg-slate-50 rounded-[20px]" />
+      <div className="SemI w-[856.33px] h-[46px] left-[119px] top-[15px] absolute text-center text-white text-opacity-60 text-[35px] font-normal font-['Roboto']">Sem I</div>
+    </div>
+    <div className="InfoboardSeperateContainer w-[1880px] h-[577px] left-[20px] top-[791px] absolute">
+      <div className="Info w-[482px] h-[492px] left-[30px] top-[60px] absolute bg-slate-50 rounded-[10px]" />
+      <div className="SemIi w-[856.34px] h-[46px] left-[119px] top-[6px] absolute text-center text-white text-opacity-60 text-[35px] font-normal font-['Roboto']">Sem II</div>
+    </div>
+  </div>
+  <div className="Title w-[1920px] h-[187.17px] left-0 top-[-14px] absolute">
+    <div className="Rectangle1 w-[1920px] h-[178.33px] left-0 top-[8.83px] absolute bg-gradient-to-b from-teal-400 via-cyan-600 to-teal-400" />
+    <div className="Rectangle11 w-[916px] h-[159px] left-[109px] top-[7px] absolute bg-stone-50 rounded-bl-[30px] rounded-br-[30px] shadow" />
+    <div className="IiitdmjCoursecollection w-[909px] h-[173px] left-[112px] top-0 absolute text-center text-slate-500 text-[67px] font-medium font-['Roboto']">IIITDMJ CourseCollection</div>
+  </div>
+  <div className="Sidebar w-[715px] h-[1080px] left-[1205px] top-0 absolute">
+    <div className="Rectangle2 w-[715px] h-[1080px] left-0 top-0 absolute bg-gradient-to-b from-sky-300 via-emerald-400 to-teal-600 rounded-tl-[30px] rounded-bl-[30px] backdrop-blur-[100px]" />
+    <div className="Rectangle3 w-[662px] h-[85.77px] left-[27px] top-[200.13px] absolute bg-stone-50 rounded-[30px]" />
+    <div className="Search w-[640px] h-[85.77px] left-[49px] top-[200.13px] absolute text-neutral-500 text-3xl font-normal font-['Roboto']">Search</div>
+    <div className="Group1 w-[662px] h-[349.12px] left-[27px] top-[307.09px] absolute">
+      <div className="Rectangle4 w-[662px] h-[349.12px] left-0 top-0 absolute bg-stone-50 rounded-[30px]" />
+      <div className="Filters w-[311.94px] h-[43.39px] left-[19.76px] top-[14.13px] absolute text-neutral-500 text-3xl font-normal font-['Roboto']">Filters</div>
+      <div className="Rectangle5 w-[406.52px] h-[57.51px] left-[19.76px] top-[70.63px] absolute bg-slate-500 bg-opacity-40 rounded-[20px]" />
+      <div className="Rectangle6 w-[406.52px] h-[57.51px] left-[19.76px] top-[141.26px] absolute bg-slate-500 bg-opacity-40 rounded-[20px]" />
+      <div className="SemAll w-[395.22px] h-[42.38px] left-[31.05px] top-[78.70px] absolute text-gray-600 text-opacity-70 text-3xl font-normal font-['Roboto']">Sem                     All</div>
+      <div className="Rectangle7 w-[406.52px] h-[57.51px] left-[19.76px] top-[211.90px] absolute bg-slate-500 bg-opacity-40 rounded-[20px]" />
+      <div className="CreditsAll w-[395.22px] h-[42.38px] left-[31.05px] top-[219.97px] absolute text-gray-600 text-opacity-70 text-3xl font-normal font-['Roboto']">Credits                 All</div>
+      <div className="DepartmentAll w-[395.22px] h-[42.38px] left-[31.05px] top-[149.34px] absolute text-gray-600 text-opacity-70 text-3xl font-normal font-['Roboto']">Department        All</div>
+    </div>
+    <div className="Group2 w-[662px] h-[266.38px] left-[27px] top-[677.40px] absolute">
+      <div className="Rectangle4 w-[662px] h-[266.38px] left-0 top-0 absolute bg-stone-50 rounded-[30px]" />
+      <div className="SeperateBy w-[311.94px] h-[33.11px] left-[19.76px] top-[10.78px] absolute text-neutral-500 text-3xl font-normal font-['Roboto']">Seperate By</div>
+      <div className="Rectangle8 w-[92px] h-[57.51px] left-[26px] top-[68.61px] absolute bg-slate-500 bg-opacity-40 rounded-[20px]" />
+      <div className="Sem w-[92px] h-[42.38px] left-[26px] top-[76.69px] absolute text-center text-gray-600 text-opacity-70 text-3xl font-normal font-['Roboto']">Sem</div>
+      <div className="Rectangle9 w-28 h-[57.51px] left-[137px] top-[68.61px] absolute bg-slate-500 bg-opacity-40 rounded-[20px]" />
+      <div className="Credits w-28 h-[42.38px] left-[137px] top-[76.69px] absolute text-center text-gray-600 text-opacity-70 text-3xl font-normal font-['Roboto']">Credits</div>
+      <div className="Rectangle10 w-[183px] h-[57.51px] left-[268px] top-[68.61px] absolute bg-slate-500 bg-opacity-40 rounded-[20px]" />
+      <div className="Department w-[183px] h-[42.38px] left-[268px] top-[76.69px] absolute text-center text-gray-600 text-opacity-70 text-3xl font-normal font-['Roboto']">Department</div>
+    </div>
+  </div>
+  <div className="Mathematics1 w-[275px] h-[45px] left-[55px] top-[275px] absolute text-center text-gray-700 text-3xl font-bold font-['Roboto Flex']">Mathematics-1</div>
+  <div className="Ns1001 w-[181px] h-[45px] left-[351px] top-[275px] absolute text-center text-gray-500 text-3xl font-bold font-['Roboto Flex']">NS1001</div>
+  <div className="Credits w-[163px] h-[45px] left-[369px] top-[661px] absolute text-center text-gray-500 text-3xl font-bold font-['Roboto Flex']">4 Credits </div>
+  <div className="Module1CalculusOfFunctionsOfOneVariableRealNumbersFunctionsSequencesLimitAndContinuityDifferentiation w-[421px] h-[293px] left-[80px] top-[372px] absolute"><span style="text-gray-600 text-xl font-semibold font-['Roboto']">Module 1:  Calculus of Functions of One Variable<br/></span><span style="text-gray-600 text-xl font-light font-['Roboto']"><br/>Real Numbers , Functions , Sequences , Limit and Continuity, Differentiation </span></div>
+</div>
